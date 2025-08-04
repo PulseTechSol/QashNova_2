5db9fd316a83177c9a5fbe74340f2a3f1d81f652
Marsad Ghanvi <marsad11223@gmail.com>
2025-08-04 14:18:20 +0500

    add meta script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ThemeRegistry from "./ThemeRegistry";
 import LayoutWrapper from "@/_components/LayoutWrapper";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Qashnova",
@@ -14,11 +15,37 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <ThemeRegistry>
-        <body>
+      <head>
+        {/* Meta Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1732463187404935');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+      <body>
+        {/* Meta Pixel NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1732463187404935&ev=PageView&noscript=1"
+          />
+        </noscript>
+        <ThemeRegistry>
           <LayoutWrapper>{children}</LayoutWrapper>
-        </body>
-      </ThemeRegistry>
+        </ThemeRegistry>
+      </body>
     </html>
   );
 }
