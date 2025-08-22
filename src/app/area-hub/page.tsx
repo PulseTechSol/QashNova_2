b0012c20193b6e508fd37cffaf79cc9b926a9dca
Marsad Ghanvi <marsad11223@gmail.com>
2025-08-22 20:02:29 +0500

    create hero HeroCity ,  AreaHero , and OurHubareasCards sections

--- src/app/area-hub/page.tsx
+++ src/app/area-hub/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import AreaHero from "./_sections/AreaHero";
+import OurHubareasCards from "./_sections/OurHubareasCards";
 
 function page() {
-  return <div>page</div>;
+  return <div><AreaHero/>
+  <OurHubareasCards/></div>;
 }
 
 export default page;
