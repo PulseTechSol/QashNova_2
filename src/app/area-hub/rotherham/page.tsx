8fce0bba87ebd1644a394e32e4716e8115610080
Marsad Ghanvi <marsad11223@gmail.com>
2025-08-26 02:48:28 +0500

    added section area hub : FaqAreahub

--- src/app/area-hub/rotherham/page.tsx
+++ src/app/area-hub/rotherham/page.tsx
@@ -5,8 +5,15 @@ import OurServicesGenaricSection from "../_sections/OurServicesGenaricSection";
 import { Box } from "@mui/material";
 import WhyQashnovaSection from "../_sections/WhyQashnovaSection";
 import CaseStudiesSection from "../_sections/CaseStudiesSection";
+import FaqAreahub from "../_sections/FaqAreahub";
+
 
 function Rotherham() {
+    const faqData = [
+    { title: "Do I need technical skills?", content: "No, we handle everything." },
+    { title: "How long to go live?", content: "Usually 10 to 14 days." },
+    { title: "Can I add features later?", content: "Yes. Scale as you grow." },
+  ];
   return (
     <>
       <Box
@@ -20,6 +27,7 @@ function Rotherham() {
         <OurServicesGenaricSection />
         <WhyQashnovaSection />
         <CaseStudiesSection />
+      <FaqAreahub items={faqData}   />
       </Box>
     </>
   );
