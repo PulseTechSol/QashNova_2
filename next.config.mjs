1dc1b6259d10988ce2b1c59eb6b526430e13b6bf
naqash <contact@pulsetechnologysolutions.co.uk>
2025-11-07 03:45:58 +0500

    add image caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply to all image types
+        source: "/:all*(png|jpg|jpeg|gif|svg|webp|ico)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+            // Cache for 1 year & don’t revalidate unless the URL changes
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
